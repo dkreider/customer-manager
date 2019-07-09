579e1b5220c4d66b30d0c5d36d3657a9ffd57002
Daniel Kreider <daniel@danielk.tech>
2019-07-09 20:09:45 -0300

    add dummy data

---
+++ src/app/customers.ts
@@ -0,0 +1,34 @@
+import { Customer } from "./models/customer";
+
+export var Customers: Array<Customer> = [
+    {
+        _id: "1234",
+        name: "Daniel K",
+        email: "daniel@daniel.com",
+        phoneNumber: "123-567-9023"
+    },
+    {
+        _id: "1235",
+        name: "Adam N",
+        email: "adam@adam.com",
+        phoneNumber: "187-896-7854"
+    },
+    {
+        _id: "1236",
+        name: "Ronald A",
+        email: "ronald@ronald.com",
+        phoneNumber: "457-781-9871"
+    },
+    {
+        _id: "1237",
+        name: "Genesis U",
+        email: "genesis@genesis",
+        phoneNumber: "564-327-8906"
+    },
+    {
+        _id: "1238",
+        name: "Rodrigo S",
+        email: "rod@rod.com",
+        phoneNumber: "125-672-9023"
+    },
+];
