0cab42d05cfa7d1bb259ea54cbd3fbe74d7011c0
Daniel Kreider <daniel@danielk.tech>
2019-07-10 13:24:13 -0300

    add some more dummy data

--- src/app/customers.ts
+++ src/app/customers.ts
@@ -2,6 +2,13 @@ import { Customer } from "./models/customer";
 
 export var Customers: Array<Customer> = [
     {
+        _id: "1243",
+        name: "Sarah W",
+        email: "sarah@sarah.com",
+        phoneNumber: "443-098-9043",
+        notes: "This is a test."
+    },
+    {
         _id: "1234",
         name: "Daniel K",
         email: "daniel@daniel.com",
@@ -34,6 +41,41 @@ export var Customers: Array<Customer> = [
         name: "Rodrigo S",
         email: "rod@rod.com",
         phoneNumber: "125-672-9023",
+        notes: "This is a test."
+    },
+    {
+        _id: "1239",
+        name: "Mr Nick",
+        email: "nick@nick.com",
+        phoneNumber: "765-092-8971",
+        notes: "This is a test."
+    },
+    {
+        _id: "1240",
+        name: "Shawn O",
+        email: "shawn@shawn.com",
+        phoneNumber: "672-098-7123",
+        notes: "This is a test."
+    },
+    {
+        _id: "1241",
+        name: "Howard N",
+        email: "howard@howard",
+        phoneNumber: "782-902-1887",
+        notes: "This is a test."
+    },
+    {
+        _id: "1242",
+        name: "Tod B",
+        email: "todb@tod.com",
+        phoneNumber: "422-987-8906",
+        notes: "This is a test."
+    },
+    {
+        _id: "1243",
+        name: "Patrick E",
+        email: "patrick@patrick.com",
+        phoneNumber: "983-021-9823",
         notes: "This is a test."
     }
 ];
