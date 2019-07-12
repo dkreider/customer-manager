c44e0bdc05ada7e464e8ec9927b53bf959613fff
Daniel Kreider <daniel@danielk.tech>
2019-07-12 17:28:24 -0300

    add a visual cue to the login button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,15 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
+  verifyingCredentials: boolean = false;
+
   loginForm = new FormGroup({
     email: new FormControl(),
     password: new FormControl()
   });
 
-  constructor(public authenticationService: AuthenticationService) { }
+  constructor(public authenticationService: AuthenticationService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {}
 
@@ -23,7 +27,11 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-    this.authenticationService.login(this.loginForm.value.email, this.loginForm.value.password);
+    this.verifyingCredentials = true;
+    this.authenticationService.login(this.loginForm.value.email, this.loginForm.value.password).catch(error => {
+      this.verifyingCredentials = false;
+      this.snackBar.open(error.message, "OK", {duration: 5000})
+    });
   }
 
 }
