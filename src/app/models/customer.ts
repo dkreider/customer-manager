ba5af9ca2e8acfaf72af705d0f1e8c1a6a648dcb
Daniel Kreider <daniel@danielk.tech>
2019-07-17 14:46:15 -0300

    add invoice model

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -1,7 +1,10 @@
+import { Invoice } from './invoice';
+
 export class Customer {
     _id: String;
     name: String;
     email: String;
     phoneNumber: String;
     notes: String;
+    invoices: Array<Invoice>
 }
---
+++ src/app/models/invoice.ts
@@ -0,0 +1,9 @@
+export class Invoice {
+    _id: String;
+    created: Date;
+    due: Date;
+    customerName: String;
+    service: String;
+    value: String;
+    additionalNotes: String;
+}
