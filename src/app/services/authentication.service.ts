5bfef072c1a0995f7ae64de89c8869d1c58fd7f9
Daniel Kreider <daniel@danielk.tech>
2019-07-17 14:41:33 -0300

    still working on authentication service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from "firebase/app";
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FirebaseAuth } from '@angular/fire';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -10,24 +10,25 @@ import { FirebaseAuth } from '@angular/fire';
 })
 export class AuthenticationService {
 
+  private authState: Observable<firebase.User>
+  private currentUser: firebase.User = null;
+
   constructor(
     private afAuth: AngularFireAuth,
-    private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar) { 
+      this.authState = this.afAuth.authState;
+    }
 
   login(username: string, password: string): Promise<auth.UserCredential>{
     return this.afAuth.auth.signInWithEmailAndPassword(username, password);
-
   }
 
-  logout(): void {
-    this.afAuth.auth.signOut();
+  logout(): Promise<void> {
+    return this.afAuth.auth.signOut();
   }
 
-  isUserLoggedIn(): boolean {
-    if (this.afAuth.auth.currentUser) {
-      return true;
-    }
-    return false;
+  isUserLoggedIn(): Observable<firebase.User> {
+    return this.afAuth.user;
   }
 
 }
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,13 +9,19 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor(public authenticationService: AuthenticationService) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
   logout(): void {
-    this.authenticationService.logout();
+    this.authenticationService.logout().then(function() {
+      window.location.reload();
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
 
 }
