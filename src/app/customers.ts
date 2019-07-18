fe85aea975936a82b953565008de949e4c61525c
Daniel Kreider <daniel@danielk.tech>
2019-07-18 18:52:22 -0300

    rebuild dummy data... again

--- src/app/customers.ts
+++ src/app/customers.ts
@@ -11,17 +11,15 @@ export var Customers: Array<Customer> = [
             _id: "1",
             created: new Date(),
             due: new Date(),
-            customerName: "Richard D",
             service: "Web Development",
-            value: "$4500",
+            value: 4500,
             additionalNotes: "Adding the 3 new pages."
         }, {
             _id: "2",
             created: new Date(),
             due: new Date(),
-            customerName: "George M",
             service: "Web App Fixes",
-            value: "$2500",
+            value: 2500,
             additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
         }]
     },
@@ -30,69 +28,153 @@ export var Customers: Array<Customer> = [
         name: "Daniel K",
         email: "daniel@daniel.com",
         phoneNumber: "123-567-9023",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1235",
         name: "Adam N",
         email: "adam@adam.com",
         phoneNumber: "187-896-7854",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1236",
         name: "Ronald A",
         email: "ronald@ronald.com",
         phoneNumber: "457-781-9871",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1237",
         name: "Genesis U",
         email: "genesis@genesis",
         phoneNumber: "564-327-8906",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1238",
         name: "Rodrigo S",
         email: "rod@rod.com",
         phoneNumber: "125-672-9023",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1239",
         name: "Mr Nick",
         email: "nick@nick.com",
         phoneNumber: "765-092-8971",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
     {
         _id: "1240",
         name: "Shawn O",
         email: "shawn@shawn.com",
         phoneNumber: "672-098-7123",
-        notes: "This is a test."
+        notes: "This is a test.",
+        invoices: [{
+            _id: "1",
+            created: new Date(),
+            due: new Date(),
+            service: "Web Development",
+            value: 4500,
+            additionalNotes: "Adding the 3 new pages."
+        }, {
+            _id: "2",
+            created: new Date(),
+            due: new Date(),
+            service: "Web App Fixes",
+            value: 2500,
+            additionalNotes: "Fixing the bug that caused a $55000 drop in profit."
+        }]
     },
-    {
-        _id: "1241",
-        name: "Howard N",
-        email: "howard@howard",
-        phoneNumber: "782-902-1887",
-        notes: "This is a test."
-    },
-    {
-        _id: "1242",
-        name: "Tod B",
-        email: "todb@tod.com",
-        phoneNumber: "422-987-8906",
-        notes: "This is a test."
-    },
-    {
-        _id: "1243",
-        name: "Patrick E",
-        email: "patrick@patrick.com",
-        phoneNumber: "983-021-9823",
-        notes: "This is a test."
-    }
 ];
